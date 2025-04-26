32f66cf47c30a6834ccc47a01789905b10f46b6d
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-04-26 22:56:37 +0400

    add postcss import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 import cssnano from 'cssnano';
 import autoprefixer from 'autoprefixer';
+import atImport from 'postcss-import';
+
 export default {
   map: 'inline',
-  plugins: [autoprefixer, cssnano],
+  plugins: [atImport, autoprefixer, cssnano],
 };
