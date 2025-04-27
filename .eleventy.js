0fd8559110530b4752b04ae25d01572c015e0a12
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-04-27 19:32:23 +0400

    add shortcodes for markdown and style logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,40 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import markdownItAttrs from 'markdown-it-attrs';
 import markdownItHeaderSections from 'markdown-it-header-sections';
+import markdownItShortcodeTag from 'markdown-it-shortcode-tag';
 import postcssPlugin from '@jgarber/eleventy-plugin-postcss';
+import fs from 'fs';
+const markdownShortcodes = {
+  ripples: {
+    render: () => {
+      return `<div class="ripples">
+        <div class="ripples__item ripples__item_n_1"></div>
+        <div class="ripples__item ripples__item_n_2"></div>
+        <div class="ripples__item ripples__item_n_3"></div>
+        <div class="ripples__item ripples__item_n_4"></div>
+      </div>`;
+    },
+  },
+  logo: {
+    render: attrs => {
+      let logoAttrs = { class: 'logo' };
+      if (Object.keys(attrs).length > 0) {
+        const { class: className } = attrs;
+        if (className) {
+          logoAttrs.class += ` ${className}`;
+        }
+      }
+      const logo = fs.readFileSync('./src/images/scalekarma-logo.svg', 'utf8');
+      return `<span ${Object.entries(logoAttrs)
+        .map(([key, value]) => `${key}="${value}"`)
+        .join(' ')}>${logo}</span>`;
+    },
+  },
+};
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPlugin(postcssPlugin);
+  eleventyConfig.addWatchTarget('./src/images/');
 
   // Add site-wide data
   eleventyConfig.addGlobalData('site', {
@@ -20,7 +48,7 @@ export default async function (eleventyConfig) {
   eleventyConfig.amendLibrary('md', lib => {
     lib.use(markdownItAttrs);
     lib.use(markdownItHeaderSections);
-
+    lib.use(markdownItShortcodeTag, markdownShortcodes);
     lib.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
       const token = tokens[idx];
       if (
