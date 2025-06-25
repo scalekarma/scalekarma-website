18abcadced5d260188c253c8b6e752204d92e917
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-06-26 01:00:11 +0400

    edit theme

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -7,9 +7,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
+		background: {
+			default: '#180427',
+		},
+		primary: {
+			main: '#cafd73',
+		},
 	},
 	typography: {
 		fontFamily: 'Uncut Sans, Arial, Helvetica, sans-serif',
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					textTransform: 'none',
+				},
+			},
+		},
 	},
 });
 
