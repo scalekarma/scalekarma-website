0f739923ef4c6728868be0b3394f52fcc66a8924
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-06-25 22:22:13 +0400

    do not touch font weight

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -10,17 +10,6 @@ const darkTheme = createTheme({
   },
   typography: {
     fontFamily: 'Uncut Sans, Arial, Helvetica, sans-serif',
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightBold: 600,
-    h1: { fontWeight: 600 },
-    h2: { fontWeight: 600 },
-    h3: { fontWeight: 600 },
-    h4: { fontWeight: 600 },
-    h5: { fontWeight: 600 },
-    h6: { fontWeight: 600 },
-    body1: { fontWeight: 400 },
-    body2: { fontWeight: 400 },
   },
 });
 
