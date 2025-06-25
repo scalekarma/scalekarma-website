3dbf1036692c7746c57dd67aa5932c9be8383b2d
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-06-26 02:06:28 +0400

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 					Streamline your shift to AI search
 				</Typography>
 				<Typography variant="subtitle1" component="p" sx={{ maxWidth: '700px' }}>
-					We're an independent research team building open solutions to help Content and SEO teams navigate the shift from conventional
+					We‘re an independent research team building open solutions to help Content and SEO teams navigate the shift from conventional
 					search to AI-generated answers.
 				</Typography>
 				<Button component={Link} href="#" variant="contained" size="large" sx={{ mt: 3 }}>
