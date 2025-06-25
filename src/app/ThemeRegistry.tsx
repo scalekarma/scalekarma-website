1538e3367b9c0eac64089624fb7eb41f756fee75
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-06-26 00:03:57 +0400

    add prettier and format jsx files

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -5,19 +5,19 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-  typography: {
-    fontFamily: 'Uncut Sans, Arial, Helvetica, sans-serif',
-  },
+	palette: {
+		mode: 'dark',
+	},
+	typography: {
+		fontFamily: 'Uncut Sans, Arial, Helvetica, sans-serif',
+	},
 });
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			{children}
+		</ThemeProvider>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,38 +7,24 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 
 export default function Home() {
-  return (
-    <Stack sx={{ minHeight: '100vh' }}>
-        <Box>
-          <svg width="60px" height="60px" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="30" cy="30" r="30" fill="#000" />
-          </svg>
-        </Box>
-        <Stack sx={{ flexGrow: 1, justifyContent: 'center', alignItems: 'start' }}>
-        <Typography
-          variant="h2"
-          component="h1"
-          gutterBottom
-        >
-          Welcome to ScaleKarma
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          component="p"
-          color="text.secondary"
-        >
-          Accelerate your business growth with AI-powered solutions.
-        </Typography>
-        <Button
-          component={Link}
-          href="/get-started"
-          variant="contained"
-          size="large"
-          sx={{ mt: 3 }}
-        >
-          Get Started
-        </Button>
-        </Stack>
-    </Stack>
-  );
+	return (
+		<Stack sx={{ minHeight: '100vh' }}>
+			<Box>
+				<svg width="60px" height="60px" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="30" cy="30" r="30" fill="#000" />
+				</svg>
+			</Box>
+			<Stack sx={{ flexGrow: 1, justifyContent: 'center', alignItems: 'start' }}>
+				<Typography variant="h2" component="h1" gutterBottom>
+					Welcome to ScaleKarma
+				</Typography>
+				<Typography variant="subtitle1" component="p" color="text.secondary">
+					Accelerate your business growth with AI-powered solutions.
+				</Typography>
+				<Button component={Link} href="/get-started" variant="contained" size="large" sx={{ mt: 3 }}>
+					Get Started
+				</Button>
+			</Stack>
+		</Stack>
+	);
 }
